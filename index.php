7c77a06d7ac22ff92ef118cbbd12f741b169e055
Rohan <rohanredkulkarni27@gmail.com>
2018-10-03 19:53:04 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+
+<form action="sendsms.php" method="get" role="form">
+    <input type="submit" value="Send SMS" />
+</form>
+</body>
+</html>
---
+++ sendsms.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__. '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
+$sid    = "AC8b143aad770462f5a97c28779a455a26"; 
+$token  = "7b9539071fb8d66b72c18d0afa00abbc"; 
+$twilio = new Client($sid, $token); 
+ 
+$message = $twilio->messages 
+                  ->create("+15512259238", // to 
+                           array( 
+                               "from" => "+18086989616",       
+                               "body" => "Hello" 
+                           ) 
+                  ); 
+ echo "Message send. Check the device";
+
+?>
