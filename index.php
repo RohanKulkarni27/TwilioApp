a9d793eddb557b10c6dff44171e95c726ecc19ad
Rohan <rohanredkulkarni27@gmail.com>
2018-10-17 16:33:28 -0400

    added readsms file

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <head>
 </head>
 <body>
-
+<?php
+echo $_SERVER['SERVER_PORT'];
+?>
 <form action="sendsms.php" method="get" role="form">
     <input type="submit" value="Send SMS" />
 </form>
---
+++ readsms.php
@@ -0,0 +1,15 @@
+<?php
+header("content-type:text/xml");
+?>
+
+<Response>
+    <Message>You can refer these medicines: 
+    Adalimumab,
+    Albuterol,
+    Quetiapine,
+    Montelukast,
+    Escitalopram,
+    Duloxetine,
+    Atorvastatin,
+    Aripiprazole</Message>
+</Response> 
