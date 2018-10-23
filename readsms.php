10cfd93ef5194826d5ce65b5577cadc1d8805b77
Rohan <rohanredkulkarni27@gmail.com>
2018-10-23 14:26:26 -0400

    Recent changes

--- readsms.php
+++ readsms.php
@@ -1,9 +1,12 @@
 <?php
 $number = $_POST['From'];
 $body = $_POST['Body'];
+if($body=='Rohan'){
+    $responseText = 'Mr. Rohan';
+}
 header("content-type:text/xml");
 ?>
 
 <Response>
-    <Message>Hello <?php echo $body ?></Message>
+    <Message>Hello <?php echo $responseText ?></Message>
 </Response> 
